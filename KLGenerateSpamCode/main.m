1aab31dd30170dac0f37d809f98dda84c784f8a8
柯磊 <kelei0017@gmail.com>
2017-08-04 13:08:32 +0800

    First commit

---
+++ KLGenerateSpamCode/main.m
@@ -0,0 +1,286 @@
+//
+//  main.m
+//  generateSpamCode
+//
+//  Created by 柯磊 on 2017/7/5.
+//  Copyright © 2017年 GAEA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#include <stdlib.h>
+
+typedef NS_ENUM(NSInteger, GSCSourceType) {
+    GSCSourceTypeClass,
+    GSCSourceTypeCategory,
+};
+
+void recursiveDirectory(NSString *directory, void(^handleFile)(NSString *mFilePath));
+void generateSpamCodeFile(NSString *outDirectory, NSString *mFilePath, GSCSourceType type);
+NSString *randomString(NSInteger length);
+void handleXcassetsFiels(NSString *directory);
+
+// 命令行修改工程目录下所有 png 资源 hash 值
+// 使用 ImageMagick 进行图片压缩，所以需要安装 ImageMagick，安装方法 brew install imagemagick
+// find . -iname "*.png" -exec echo {} \; -exec convert {} {} \;
+// or
+// find . -iname "*.png" -exec echo {} \; -exec convert {} -quality 95 {} \;
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSArray<NSString *> *arguments = [[NSProcessInfo processInfo] arguments];
+        if (!arguments || arguments.count <= 1) {
+            printf("缺少工程目录参数\n");
+            return 1;
+        }
+        if (arguments.count <= 2) {
+            printf("缺少任务参数 -spamCodeOut or -handleXcassets\n");
+            return 1;
+        }
+        
+        BOOL isDirectory = NO;
+        NSString *projectDirString = nil;
+        NSString *outDirString = nil;
+        BOOL needHandleXcassets = NO;
+        
+        NSFileManager *fm = [NSFileManager defaultManager];
+        for (NSInteger i = 1; i < arguments.count; i++) {
+            NSString *argument = arguments[i];
+            if (i == 1) {
+                projectDirString = argument;
+                if (![fm fileExistsAtPath:projectDirString isDirectory:&isDirectory]) {
+                    printf("%s不存在\n", [projectDirString UTF8String]);
+                    return 1;
+                }
+                if (!isDirectory) {
+                    printf("%s不是目录\n", [projectDirString UTF8String]);
+                    return 1;
+                }
+                continue;
+            }
+            if ([argument isEqualToString:@"-spamCodeOut"]) {
+                outDirString = arguments[++i];
+                if ([fm fileExistsAtPath:outDirString isDirectory:&isDirectory]) {
+                    if (!isDirectory) {
+                        printf("%s 已存在但不是文件夹，需要传入一个输出文件夹目录\n", [outDirString UTF8String]);
+                        return 1;
+                    }
+                } else {
+                    NSError *error = nil;
+                    if (![fm createDirectoryAtPath:outDirString withIntermediateDirectories:YES attributes:nil error:&error]) {
+                        printf("创建输出目录失败，请确认 -spamCodeOut 之后接的是一个“输出文件夹目录”参数，错误信息如下：\n传入的输出文件夹目录：%s\n%s", [outDirString UTF8String], [error.localizedDescription UTF8String]);
+                        return 1;
+                    }
+                }
+                continue;
+            }
+            if ([argument isEqualToString:@"-handleXcassets"]) {
+                needHandleXcassets = YES;
+            }
+        }
+        
+        
+        recursiveDirectory(projectDirString, ^(NSString *mFilePath){
+            generateSpamCodeFile(outDirString, mFilePath, GSCSourceTypeClass);
+            generateSpamCodeFile(outDirString, mFilePath, GSCSourceTypeCategory);
+        });
+        
+        if (needHandleXcassets) {
+            handleXcassetsFiels(projectDirString);
+        }
+    }
+    return 0;
+}
+
+void recursiveDirectory(NSString *directory, void(^handleFile)(NSString *mFilePath)) {
+    NSFileManager *fm = [NSFileManager defaultManager];
+    NSArray<NSString *> *files = [fm contentsOfDirectoryAtPath:directory error:nil];
+    BOOL isDirectory;
+    for (NSString *filePath in files) {
+        NSString *path = [directory stringByAppendingPathComponent:filePath];
+        if ([fm fileExistsAtPath:path isDirectory:&isDirectory] && isDirectory) {
+            recursiveDirectory(path, handleFile);
+            continue;
+        }
+        NSString *fileName = filePath.lastPathComponent;
+        if (![fileName hasSuffix:@".h"]) continue;
+        fileName = [fileName stringByDeletingPathExtension];
+        
+        NSString *mFileName = [fileName stringByAppendingPathExtension:@"m"];
+        if ([files containsObject:mFileName]) {
+            handleFile([directory stringByAppendingPathComponent:mFileName]);
+        }
+    }
+}
+
+static NSString *const kClassCategoryName = @"GSC_CLASS_GenerateSpamCode";
+static NSString *const kCategoryCategoryName = @"GSC_CATEGORY_GenerateSpamCode";
+static NSString *const kHSystemClassFileTemplate = @"\
+#import <Foundation/Foundation.h>\n\
+#import <UIKit/UIKit.h>\n\
+@interface %@ (%@)\n\
+%@\n\
+@end\n";
+static NSString *const kHCustomClassFileTemplate = @"\
+#import \"%@.h\"\n\
+@interface %@ (%@)\n\
+%@\n\
+@end\n";
+static NSString *const kMClassFileTemplate = @"\
+#import \"%@+%@.h\"\n\
+@implementation %@ (%@)\n\
+%@\n\
+@end\n";
+void generateSpamCodeFile(NSString *outDirectory, NSString *mFilePath, GSCSourceType type) {
+    NSString *mFileContent = [NSString stringWithContentsOfFile:mFilePath encoding:NSUTF8StringEncoding error:nil];
+    NSString *regexStr;
+    switch (type) {
+        case GSCSourceTypeClass:
+            regexStr = @" *@implementation +(\\w+)[^(]*\\n(?:.|\\n)+?@end";
+            break;
+        case GSCSourceTypeCategory:
+            regexStr = @" *@implementation *(\\w+) *\\((\\w+)\\)(?:.|\\n)+?@end";
+            break;
+    }
+    
+    NSRegularExpression *expression = [NSRegularExpression regularExpressionWithPattern:regexStr options:NSRegularExpressionUseUnicodeWordBoundaries error:nil];
+    NSArray<NSTextCheckingResult *> *matches = [expression matchesInString:mFileContent options:0 range:NSMakeRange(0, mFileContent.length)];
+    if (matches.count <= 0) return;
+    
+    [matches enumerateObjectsUsingBlock:^(NSTextCheckingResult * _Nonnull impResult, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSString *className = [mFileContent substringWithRange:[impResult rangeAtIndex:1]];
+        NSString *categoryName = nil;
+        if (impResult.numberOfRanges >= 3) {
+            categoryName = [mFileContent substringWithRange:[impResult rangeAtIndex:2]];
+        }
+        
+        if (type == GSCSourceTypeClass) {
+            // 如果该类型没有公开，只在 .m 文件中使用，则不处理
+            NSString *hFilePath = [mFilePath.stringByDeletingPathExtension stringByAppendingPathExtension:@"h"];
+            NSString *hFileContent = [NSString stringWithContentsOfFile:hFilePath encoding:NSUTF8StringEncoding error:nil];
+            
+            NSString *regexStr = [NSString stringWithFormat:@"\\b%@\\b", className];
+            NSRange range = [hFileContent rangeOfString:regexStr options:NSRegularExpressionSearch];
+            if (range.location == NSNotFound) {
+                return;
+            }
+        }
+        
+        NSString *implementation = [mFileContent substringWithRange:impResult.range];
+        NSRegularExpression *expression = [NSRegularExpression regularExpressionWithPattern:@" *([-+]) *\\(" options:NSRegularExpressionUseUnicodeWordBoundaries error:nil];
+        NSArray<NSTextCheckingResult *> *matches = [expression matchesInString:implementation options:0 range:NSMakeRange(0, implementation.length)];
+        if (matches.count <= 0) return;
+        
+        NSMutableString *hFileMethodsString = [NSMutableString string];
+        NSMutableString *mFileMethodsString = [NSMutableString string];
+        [matches enumerateObjectsUsingBlock:^(NSTextCheckingResult * _Nonnull matche, NSUInteger idx, BOOL * _Nonnull stop) {
+            NSString *symbol = [implementation substringWithRange:[matche rangeAtIndex:1]];
+            NSString *methodName = [@"gsc_ppp_" stringByAppendingString:randomString(arc4random_uniform(16) + 16)];
+            [hFileMethodsString appendFormat:@"%@ (NSArray *)%@;\n", symbol, methodName];
+            
+            [mFileMethodsString appendFormat:@"%@ (NSArray *)%@ {\n", symbol, methodName];
+            [mFileMethodsString appendString:@"    NSMutableArray *array = [NSMutableArray array];\n"];
+            NSInteger numCount = arc4random_uniform(40) + 1;
+            for (NSInteger j = 0; j < numCount; j++) {
+                [mFileMethodsString appendFormat:@"    [array addObject:@(%ld)];\n", (long)arc4random_uniform(999)];
+            }
+            [mFileMethodsString appendString:@"    return array;\n"];
+            [mFileMethodsString appendString:@"}\n"];
+        }];
+        
+        NSString *newCategoryName;
+        switch (type) {
+            case GSCSourceTypeClass:
+                newCategoryName = kClassCategoryName;
+                break;
+            case GSCSourceTypeCategory:
+                newCategoryName = [NSString stringWithFormat:@"%@_%@", kCategoryCategoryName, categoryName];
+                break;
+        }
+        
+        NSString *fileName = [NSString stringWithFormat:@"%@+%@.h", className, newCategoryName];
+        NSString *fileContent;
+        if ([className hasPrefix:@"NS"] || [className hasPrefix:@"UI"]) {
+            fileContent = [NSString stringWithFormat:kHSystemClassFileTemplate, className, newCategoryName, hFileMethodsString];
+        } else {
+            NSString *soureHFileName = mFilePath.lastPathComponent.stringByDeletingPathExtension;
+            fileContent = [NSString stringWithFormat:kHCustomClassFileTemplate, soureHFileName, className, newCategoryName, hFileMethodsString];
+        }
+        [fileContent writeToFile:[outDirectory stringByAppendingPathComponent:fileName] atomically:YES encoding:NSUTF8StringEncoding error:nil];
+        
+        fileName = [NSString stringWithFormat:@"%@+%@.m", className, newCategoryName];
+        fileContent = [NSString stringWithFormat:kMClassFileTemplate, className, newCategoryName, className, newCategoryName, mFileMethodsString];
+        [fileContent writeToFile:[outDirectory stringByAppendingPathComponent:fileName] atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    }];
+    
+    
+}
+
+static const NSString *kRandomAlphabet = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+NSString *randomString(NSInteger length) {
+    NSMutableString *ret = [NSMutableString stringWithCapacity:length];
+    for (int i = 0; i < length; i++) {
+        [ret appendFormat:@"%C", [kRandomAlphabet characterAtIndex:arc4random_uniform((uint32_t)[kRandomAlphabet length])]];
+    }
+    return ret;
+}
+
+void handleXcassetsFiels(NSString *directory) {
+    NSFileManager *fm = [NSFileManager defaultManager];
+    NSArray<NSString *> *files = [fm contentsOfDirectoryAtPath:directory error:nil];
+    BOOL isDirectory;
+    for (NSString *fileName in files) {
+        NSString *filePath = [directory stringByAppendingPathComponent:fileName];
+        if ([fm fileExistsAtPath:filePath isDirectory:&isDirectory] && isDirectory) {
+            handleXcassetsFiels(filePath);
+            continue;
+        }
+        if (![fileName isEqualToString:@"Contents.json"]) continue;
+        NSString *contentsDirectoryName = filePath.stringByDeletingLastPathComponent.lastPathComponent;
+        if (![contentsDirectoryName hasSuffix:@".imageset"]) continue;
+        
+        NSString *fileContent = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+        if (!fileContent) continue;
+        
+        NSMutableArray<NSString *> *processedImageFileNameArray = @[].mutableCopy;
+        static NSString * const regexStr = @"\"filename\" *: *\"(.*)?\"";
+        NSRegularExpression *expression = [NSRegularExpression regularExpressionWithPattern:regexStr options:NSRegularExpressionUseUnicodeWordBoundaries error:nil];
+        NSArray<NSTextCheckingResult *> *matches = [expression matchesInString:fileContent options:0 range:NSMakeRange(0, fileContent.length)];
+        while (matches.count > 0) {
+            NSInteger i = 0;
+            NSString *imageFileName = nil;
+            do {
+                if (i >= matches.count) {
+                    i = -1;
+                    break;
+                }
+                imageFileName = [fileContent substringWithRange:[matches[i] rangeAtIndex:1]];
+                i++;
+            } while ([processedImageFileNameArray containsObject:imageFileName]);
+            if (i < 0) break;
+            
+            NSString *imageFilePath = [filePath.stringByDeletingLastPathComponent stringByAppendingPathComponent:imageFileName];
+            if ([fm fileExistsAtPath:imageFilePath]) {
+                NSString *newImageFileName = [randomString(10) stringByAppendingPathExtension:imageFileName.pathExtension];
+                NSString *newImageFilePath = [filePath.stringByDeletingLastPathComponent stringByAppendingPathComponent:newImageFileName];
+                while ([fm fileExistsAtPath:newImageFileName]) {
+                    newImageFileName = [randomString(10) stringByAppendingPathExtension:imageFileName.pathExtension];
+                    newImageFilePath = [filePath.stringByDeletingLastPathComponent stringByAppendingPathComponent:newImageFileName];
+                }
+                
+                NSError *error = nil;
+                [fm moveItemAtPath:imageFilePath toPath:newImageFilePath error:&error];
+                assert(!error);
+                
+                fileContent = [fileContent stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"\"%@\"", imageFileName]
+                                                                     withString:[NSString stringWithFormat:@"\"%@\"", newImageFileName]];
+                [fileContent writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+                
+                [processedImageFileNameArray addObject:newImageFileName];
+            } else {
+                [processedImageFileNameArray addObject:imageFileName];
+            }
+            
+            matches = [expression matchesInString:fileContent options:0 range:NSMakeRange(0, fileContent.length)];
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# KLGenerateSpamCode
-垃圾代码生成器
+# KLGenerateSpamCode 垃圾代码生成器
+本工具用于应对苹果对重复应用的审核（Guideline 4.3 Design Spam），避免苹果机审检测概率。
+
+## 主要功能
+1. 扫描工程中的代码，生成同等数量的 Category 文件，文件中及是同等方法数量的垃圾代码，垃圾代码方法名随机。
+1. 修改 xxx.xcassets 文件夹中的 png 资源文件名。
+
+## 使用
+1. 下载源码。
+1. 用 Xcode 打开工程并配置参数。如图![配置参数](https://github.com/klaus01/KLGenerateSpamCode/raw/master/images/p1.png)￼
+1. 运行，生成垃圾代码，处理 png 文件名。
+1. 将生成的垃圾代码拖入工程中。
+
+### 参数说明
+- **工程源文件目录**（如：`/Users/kelei/Documents/work/git/projectName/source`）
+- **-spamCodeOut** 后面跟垃圾代码输出目录（如：`-spamCodeOut /Users/kelei/Desktop/GSCCategory`）
+- **-handleXcassets** 修改`xxx.xcassets`文件夹中的 png 资源文件名，同时也`Contents.json`文件中的关联名称，不会影响代码中使用图片。
+
+## 已知问题
+- 生成的垃圾代码文件可能是 .m 文件中实现的私有类，编译垃圾代码可能会报错，删除该垃圾代码 .h .m 文件及可。
+
+## 另外修改图片 hash 值的方法
+使用 [ImageMagick](http://www.imagemagick.org/) 对 png 图片做轻量压缩，及不损失图片质量，又可改变图片文件 hash 值。方法：
+1. 安装 ImageMagick，`brew install imagemagick`
+2. 压缩工程目录下所有 png 文件，`find . -iname "*.png" -exec echo {} \; -exec convert {} {} \;`
+
