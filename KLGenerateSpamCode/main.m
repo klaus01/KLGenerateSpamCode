fccfcdab3788a0a758f48a08f52de7be3424841a
柯磊 <kelei0017@gmail.com>
2017-12-19 10:54:19 +0800

    修改工程名时，同时修改*.entitlements、*-Bridging-Header.h、*-Swift.h 和 Podfile.target 名称。 #1

--- KLGenerateSpamCode/main.m
+++ KLGenerateSpamCode/main.m
@@ -570,6 +570,62 @@ void deleteComments(NSString *directory) {
 
 #pragma mark - 修改工程名
 
+void resetEntitlementsFileName(NSString *projectPbxprojFilePath, NSString *oldName, NSString *newName) {
+    NSString *rootPath = projectPbxprojFilePath.stringByDeletingLastPathComponent.stringByDeletingLastPathComponent;
+    NSMutableString *fileContent = [NSMutableString stringWithContentsOfFile:projectPbxprojFilePath encoding:NSUTF8StringEncoding error:nil];
+    
+    NSString *regularExpression = @"CODE_SIGN_ENTITLEMENTS = \"?([^\";]+)";
+    NSRegularExpression *expression = [NSRegularExpression regularExpressionWithPattern:regularExpression options:0 error:nil];
+    NSArray<NSTextCheckingResult *> *matches = [expression matchesInString:fileContent options:0 range:NSMakeRange(0, fileContent.length)];
+    [matches enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(NSTextCheckingResult * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSString *entitlementsPath = [fileContent substringWithRange:[obj rangeAtIndex:1]];
+        NSString *entitlementsName = entitlementsPath.lastPathComponent.stringByDeletingPathExtension;
+        if (![entitlementsName isEqualToString:oldName]) return;
+        entitlementsPath = [rootPath stringByAppendingPathComponent:entitlementsPath];
+        if (![[NSFileManager defaultManager] fileExistsAtPath:entitlementsPath]) return;
+        NSString *newPath = [entitlementsPath.stringByDeletingLastPathComponent stringByAppendingPathComponent:[newName stringByAppendingPathExtension:@"entitlements"]];
+        renameFile(entitlementsPath, newPath);
+    }];
+}
+
+void resetBridgingHeaderFileName(NSString *projectPbxprojFilePath, NSString *oldName, NSString *newName) {
+    NSString *rootPath = projectPbxprojFilePath.stringByDeletingLastPathComponent.stringByDeletingLastPathComponent;
+    NSMutableString *fileContent = [NSMutableString stringWithContentsOfFile:projectPbxprojFilePath encoding:NSUTF8StringEncoding error:nil];
+    
+    NSString *regularExpression = @"SWIFT_OBJC_BRIDGING_HEADER = \"?([^\";]+)";
+    NSRegularExpression *expression = [NSRegularExpression regularExpressionWithPattern:regularExpression options:0 error:nil];
+    NSArray<NSTextCheckingResult *> *matches = [expression matchesInString:fileContent options:0 range:NSMakeRange(0, fileContent.length)];
+    [matches enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(NSTextCheckingResult * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSString *entitlementsPath = [fileContent substringWithRange:[obj rangeAtIndex:1]];
+        NSString *entitlementsName = entitlementsPath.lastPathComponent.stringByDeletingPathExtension;
+        if (![entitlementsName isEqualToString:oldName]) return;
+        entitlementsPath = [rootPath stringByAppendingPathComponent:entitlementsPath];
+        if (![[NSFileManager defaultManager] fileExistsAtPath:entitlementsPath]) return;
+        NSString *newPath = [entitlementsPath.stringByDeletingLastPathComponent stringByAppendingPathComponent:[newName stringByAppendingPathExtension:@"h"]];
+        renameFile(entitlementsPath, newPath);
+    }];
+}
+
+void replacePodfileContent(NSString *filePath, NSString *oldString, NSString *newString) {
+    NSMutableString *fileContent = [NSMutableString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+    
+    NSString *regularExpression = [NSString stringWithFormat:@"target +'%@", oldString];
+    NSRegularExpression *expression = [NSRegularExpression regularExpressionWithPattern:regularExpression options:0 error:nil];
+    NSArray<NSTextCheckingResult *> *matches = [expression matchesInString:fileContent options:0 range:NSMakeRange(0, fileContent.length)];
+    [matches enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(NSTextCheckingResult * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [fileContent replaceCharactersInRange:obj.range withString:[NSString stringWithFormat:@"target '%@", newString]];
+    }];
+    
+    regularExpression = [NSString stringWithFormat:@"project +'%@.", oldString];
+    expression = [NSRegularExpression regularExpressionWithPattern:regularExpression options:0 error:nil];
+    matches = [expression matchesInString:fileContent options:0 range:NSMakeRange(0, fileContent.length)];
+    [matches enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(NSTextCheckingResult * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [fileContent replaceCharactersInRange:obj.range withString:[NSString stringWithFormat:@"project '%@.", newString]];
+    }];
+    
+    [fileContent writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+}
+
 void replaceProjectFileContent(NSString *filePath, NSString *oldString, NSString *newString) {
     NSMutableString *fileContent = [NSMutableString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
     
@@ -583,6 +639,8 @@ void replaceProjectFileContent(NSString *filePath, NSString *oldString, NSString
     [fileContent writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:nil];
 }
 
+void modifyFilesClassName(NSString *sourceCodeDir, NSString *oldClassName, NSString *newClassName);
+
 void modifyProjectName(NSString *projectDir, NSString *oldName, NSString *newName) {
     NSString *sourceCodeDirPath = [projectDir stringByAppendingPathComponent:oldName];
     NSString *xcodeprojFilePath = [sourceCodeDirPath stringByAppendingPathExtension:@"xcodeproj"];
@@ -591,9 +649,13 @@ void modifyProjectName(NSString *projectDir, NSString *oldName, NSString *newNam
     NSFileManager *fm = [NSFileManager defaultManager];
     BOOL isDirectory;
     
-    // 改源代码文件夹名称
-    if ([fm fileExistsAtPath:sourceCodeDirPath isDirectory:&isDirectory] && isDirectory) {
-        renameFile(sourceCodeDirPath, [projectDir stringByAppendingPathComponent:newName]);
+    // old-Swift.h > new-Swift.h
+    modifyFilesClassName(projectDir, [oldName stringByAppendingString:@"-Swift.h"], [newName stringByAppendingString:@"-Swift.h"]);
+    
+    // 改 Podfile 中的工程名
+    NSString *podfilePath = [projectDir stringByAppendingPathComponent:@"Podfile"];
+    if ([fm fileExistsAtPath:podfilePath isDirectory:&isDirectory] && !isDirectory) {
+        replacePodfileContent(podfilePath, oldName, newName);
     }
     
     // 改工程文件内容
@@ -601,6 +663,8 @@ void modifyProjectName(NSString *projectDir, NSString *oldName, NSString *newNam
         // 替换 project.pbxproj 文件内容
         NSString *projectPbxprojFilePath = [xcodeprojFilePath stringByAppendingPathComponent:@"project.pbxproj"];
         if ([fm fileExistsAtPath:projectPbxprojFilePath]) {
+            resetBridgingHeaderFileName(projectPbxprojFilePath, [oldName stringByAppendingString:@"-Bridging-Header"], [newName stringByAppendingString:@"-Bridging-Header"]);
+            resetEntitlementsFileName(projectPbxprojFilePath, oldName, newName);
             replaceProjectFileContent(projectPbxprojFilePath, oldName, newName);
         }
         // 替换 project.xcworkspace/contents.xcworkspacedata 文件内容
@@ -631,6 +695,11 @@ void modifyProjectName(NSString *projectDir, NSString *oldName, NSString *newNam
         }
         // 改名工程文件
         renameFile(xcworkspaceFilePath, [[projectDir stringByAppendingPathComponent:newName] stringByAppendingPathExtension:@"xcworkspace"]);
+    }
+    
+    // 改源代码文件夹名称
+    if ([fm fileExistsAtPath:sourceCodeDirPath isDirectory:&isDirectory] && isDirectory) {
+        renameFile(sourceCodeDirPath, [projectDir stringByAppendingPathComponent:newName]);
     }
 }
 
--- README.md
+++ README.md
@@ -23,7 +23,7 @@ $ ./GenerateSpamCode \
 
 ### 参数说明
 - *(必填)* **源码文件夹绝对路径**（如：`/Users/kelei/Documents/work/git/projectName/source`）
-- **-modifyProjectName [原名称]>[新名称]** 修改工程名。程序会修改`源码文件夹绝对路径`名称，`原名称.xcodeproj`和`原名称.xcworkspace`的名称和内容。
+- **-modifyProjectName [原名称]>[新名称]** 修改工程名。程序会修改`原名称-Swift.h`、`Podfile`、`原名称-Bridging-Header.h`、`源码文件夹绝对路径`、`原名称.xcodeproj`和`原名称.xcworkspace`的名称和内容。*`Podfile`被修改后需要手动`pod install`*
 - **-modifyClassNamePrefix [工程文件 xcodeproj 绝对路径] [原前缀]>[新前缀]** 修改源代码类名前缀。程序会扫描`源码文件夹绝对路径`下的 .h .swift 文件，修改文件名，修改使用该类名的代码，修改`工程文件`中的文件名。文件名有`原前缀`的会修改成`新前缀`，如：`原前缀ViewController`变成`新前缀ViewController`；没有`原前缀`的会增加`新前缀`，如：`ViewController`变成`新前缀ViewController`。
 - **-spamCodeOut [垃圾代码文件输出目录] [垃圾代码方法增加的参数名]** 生成垃圾代码。程序会扫描`源码文件夹绝对路径`下的 .m .swift 文件中的类和方法，并生成`category`和`extension`文件，文件中的方法是在代码原方法的基础上增加`垃圾代码方法增加的参数名`参数。如：`-spamCodeOut /dir AppLog`，`- (void)setupKeys {}`>`- (void)setupKeysAppLog:(NSString *)appLog {}`，`- (void)foo:(NSString *)str {}`>`- (void)foo:(NSString *)str appLog:(NSString *)appLog {}`
 - **-ignoreDirNames [忽略文件夹名称字符串]** 忽略这些文件夹，对`-modifyClassNamePrefix`和`-spamCodeOut`参数有效。目前只会忽略`源码文件夹绝对路径`下一级的这些目录。如：`/p/s -ignoreDirNames categorys`，那么`/p/s/categorys`会被忽略，但`/p/s/viewControllers/categorys`不会忽略。
